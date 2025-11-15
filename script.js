0e8e70746f7d1a149a05026a5c39d1d2370a2dd5
rathinakumar-dev <rathinakumar825@gmail.com>
2025-11-15 19:16:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+lucide.createIcons();
+
+// Navbar start
+const menuBtn = document.getElementById("menu-btn");
+const closeMenu = document.getElementById("close-menu");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.remove("hidden");
+});
+
+closeMenu.addEventListener("click", () => {
+  mobileMenu.classList.add("hidden");
+});
+
+mobileMenu.addEventListener("click", (e) => {
+  if (e.target === mobileMenu) {
+    mobileMenu.classList.add("hidden");
+  }
+});
+// Navbar end
+
+ 
